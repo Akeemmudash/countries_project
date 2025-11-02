c2b3dfc9b0103a2d545689052bb763db654f9bdf
Akeem Mudashiru <akeemmudash@gmail.com>
2025-11-02 18:12:47 +0100

    feat: Fetch and display countries from API
    This commit replaces the static "Hello, world!" page with a dynamic application that fetches and displays a list of all countries from the REST Countries API.
    
    Key changes include:
    - Using the `fetch` API to retrieve country data (name and flag).
    - Adding a loading spinner to `index.html` to provide user feedback while data is being fetched.
    - Dynamically generating and injecting a Bootstrap card for each country into the DOM.
    - Implementing error handling to display an alert if the API request fails.
    - Removing the loader element once the fetch operation is complete, regardless of the outcome.

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-const myPromise = new Promise((resolve, reject) =>
-  setTimeout(() => {
-    reject("did not work");
-  }, 10000)
-);
-myPromise
+const wrapper = document.querySelector("#countriesWrapper");
+const loader = document.querySelector("#loader");
+
+
+fetch("https://restcountries.com/v3.1/all?fields=name,flags")
+  .then((response) => response.json())
   .then((data) => {
-    console.log(data);
+    data.forEach((country) => {
+      const countryCard = `
+      <div class="col-sm-6 col-lg-4">
+      <div class="card">
+  <img src="${country.flags.svg}" style="height:200px" class="card-img-top object-fit-cover" alt="${country.flags.alt}">
+  <div class="card-body">
+    <h5 class="card-title">${country.name.common}</h5>
+    <p class="card-text">${country.name.official}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>`;
+      wrapper.innerHTML = wrapper.innerHTML + countryCard;
+    });
   })
   .catch((error) => {
-    console.error(error);
-  });
-
-  
-let myNewName;
-setTimeout(() => myNewName.th , 100);
-console.log(`My new is ${myNewName}`);
-
-
+    wrapper.innerHTML += `<div class="alert alert-danger" role="alert">
+  ${error.message}
+</div>`;
+  })
+  .finally(() => loader.remove());
